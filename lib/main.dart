9e94f3f11afb9dd5ac5a0ec56bbe49c71d94d622
Marach8 <nnannamarach4@gmail.com>
2025-09-21 20:18:27 +0100

    initial commit for this project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const UITask());
+}
+
+class UITask extends StatelessWidget {
+  const UITask({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
